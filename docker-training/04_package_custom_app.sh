aa0a96173069cd992b29ab6516fc5a5fe3791f56
Gendalfion <amvrosovalex@gmail.com>
2018-09-19 23:54:35 +0800

    docker training #3

---
+++ docker-training/04_package_custom_app.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ./docker-training/linux_tweet_app/
+
+cat ./Dockerfile
+
+# just for convenience export you docker id (login to https://cloud.docker.com)
+export DOCKERID=<your docker id>
+
+# Use the docker image build command to create a new Docker image using the instructions in the Dockerfile
+docker image build --tag $DOCKERID/lin wux_tweet_app:1.0 .
+
+# Run the built image
+docker container run --detach --publish 80:80 --name linux_tweet_app $DOCKERID/linux_tweet_app:1.0
